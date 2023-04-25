f1da518685bc27b6f43f92af2f94b55cb721a188
LassilaJoni <joni@jonilassila.com>
2023-04-25 13:39:38 +0300

    Added Command assignment.

---
+++ Command/src/Screen.java
@@ -0,0 +1,9 @@
+public class Screen {
+    public void up() {
+        System.out.println("valkokangas ylös");
+    }
+
+    public void down() {
+        System.out.println("valkokangas alas");
+    }
+}
---
+++ Command/src/ScreenUpCommand.java
@@ -0,0 +1,12 @@
+public class ScreenUpCommand implements Command {
+    private Screen screen;
+
+    public ScreenUpCommand(Screen screen) {
+        this.screen = screen;
+    }
+
+    @Override
+    public void execute() {
+        screen.up();
+    }
+}
